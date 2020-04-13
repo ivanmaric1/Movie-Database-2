60fd1feb88fdf4c9f72bd5571a491bf1bba967e4
Ivan <js314head@gmail.com>
2020-04-13 20:00:46 +0200

    program dovršen

--- program.js
+++ program.js
@@ -20,10 +20,12 @@ function spremiPodatke() {
     prikažiFilmove()
 }
 
-function prikažiFilmove() {
+function prikažiFilmove(filter) {
     if(localStorage.getItem('movies') !== null) {
         let movies = JSON.parse(localStorage.getItem('movies'))
-        movies.forEach((item) => {
+        let filtered = movies.filter(r => r.info.name.includes(filter))
+
+        filtered.forEach((item) => {
             let newElement = document.createElement('li')
             let text = ` - `
             for(let key in item.info) {
@@ -62,16 +64,11 @@ function brišiFilm(e) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
+function filtriraj() {
+    let filter = document.querySelector('.search__input').value
+    document.querySelector('.output__list').innerHTML = ''
+    prikažiFilmove(filter)
+}
 
 
 
@@ -81,6 +78,8 @@ function brišiFilm(e) {
 // Event - spremanje filma
 document.querySelector('.btn--entry').addEventListener('click', spremiPodatke)
 // Prikaz filmova
-document.addEventListener('DOMContentLoaded', prikažiFilmove)
+document.addEventListener('DOMContentLoaded', prikažiFilmove(''))
 // Brisanje filma
 document.querySelector('.output__list').addEventListener('click', brišiFilm)
+// Filtriranje
+document.querySelector('.btn--search').addEventListener('click', filtriraj)
